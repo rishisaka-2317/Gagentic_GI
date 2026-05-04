4e196ed5b3c1a38499aad9fdaaf377259ff376f3
rishisaka-2317 <rishi.saka@gmail.com>
2026-05-04 00:57:56 -0500

    Fix LangGraph input format and agent invoke issue

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+
+from langchain.tools import tool
+
+from langchain_core.messages import HumanMessage
+
+from langchain_openai import ChatOpenAI
+
+
+
+
 import os
 load_dotenv()
 
+
+def search(query:str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+         The search result
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"      
+
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
+
 def main():
     print("Hello from gagentic-gi!")
-    print(os.environ.get("OPENAI_API_KEY"))
+    result = agent.invoke({"messages": HumanMessage(content="Whats the weather in Tokyo")})
+    print(result)
 
 
 if __name__ == "__main__":
     main()
+
+
